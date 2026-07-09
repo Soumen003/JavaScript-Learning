4ac82424890ea13ff297c779a6244ee0ba7cdb29
Soumen Barick <baricksoumen5@gmail.com>
2026-07-10 00:24:03 +0530

    Initial commit

---
+++ 02_datatypes.js
@@ -0,0 +1,22 @@
+// alert() // we are using node js not browser
+
+
+ let name  = "Hitesh";
+ let age = 16 
+ let  isLoggedin = false;
+
+
+
+// number => 2 to power 12
+// bignit
+// string 
+// boolean => true/ false 
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+
+//object
+console.log(typeof age); // number
+console.log(typeof null) // object
