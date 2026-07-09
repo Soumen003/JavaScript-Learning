ac707bd2ac4d21b8c222a53729a669ba3127d768
Soumen Barick <baricksoumen5@gmail.com>
2026-07-10 00:51:34 +0530

    03_conversionOperations

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -13,7 +13,7 @@ console.log(valueInNumber); // NaN => Not a Number
 // "33abc" => NaN
 // true => 1 ; 
 
-let isLoggedIn = 1 
+let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
@@ -23,7 +23,43 @@ console.log(booleanIsLoggedIn);
 // 1 => true , 0 => False
 
 
-let someNumber = 33 
+let someNumber = 33
 
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber);
+
+
+// ******************** Operations *************************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "soumen";
+let str2 = "Barick";
+
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true); // 1
+console.log(+""); // 0
+
+let num1 , num2, num3 
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
