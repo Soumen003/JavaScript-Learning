3e44a70038429f4ac3249796955425dcb9e60eac
Soumen Barick <baricksoumen5@gmail.com>
2026-07-11 00:11:14 +0530

    json

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,32 @@
+// DESTRUCTURING 
+
+
+
+const course = {
+    coursename : "Javascript",
+    price : "999",
+    courseInstructor : "Soumen"
+}
+
+const {courseInstructor : instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+
+
+// JSON 
+
+// {
+//     "name" : "S", 
+//     "price" : "free"
+// }
+
+
+
+
+// [
+//     {},
+//     {}
+// ]
