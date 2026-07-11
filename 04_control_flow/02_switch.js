61498ad8fd9a9e5c9c3c8493560892e4d42110ab
Soumen Barick <baricksoumen5@gmail.com>
2026-07-11 21:01:05 +0530

    Switch Statement

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,57 @@
+// Switch Statement 
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3 
+
+switch (month) {
+    case 1: 
+       console.log("January")       
+        break;
+    case 2: 
+       console.log("Febuary")       
+        break;
+    case 3: 
+       console.log("March")       
+        break;
+    case 4: 
+       console.log("April")       
+        break;
+
+    default:
+        console.log("Invalid Input");        
+        break;
+}
+
+
+
+
+const month = "march" 
+
+switch (month) {
+    case "Jan": 
+       console.log("January")       
+        break;
+    case "Feb": 
+       console.log("Febuary")       
+        break;
+    case "Mar": 
+       console.log("March")       
+        break;
+    case "Apr": 
+       console.log("April")       
+        break;
+
+    default:
+        console.log("Invalid Input");        
+        break;
+}
+
+
