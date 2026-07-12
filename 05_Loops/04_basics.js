477ecfd589810a6228fb2a4dce878b4e18fed149
Soumen Barick <baricksoumen5@gmail.com>
2026-07-12 20:34:32 +0530

    For In Loops

---
+++ 05_Loops/04_basics.js
@@ -0,0 +1,18 @@
+const myObject = {
+    js : 'Javascript',
+    cpp : 'C++',
+    rb : 'ruby'
+}
+// For in Loops
+for (const key in myObject) {
+//     console.log(`${key} ShortCut is for ${myObject[key]}`)
+ }
+
+
+const programming = ["js", "py", "java"]
+
+for (const key in programming) {
+    console.log(programming[key]); 
+}
+
+
