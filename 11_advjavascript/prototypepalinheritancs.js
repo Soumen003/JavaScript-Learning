1e1d08c828844611faa53b67f54217a69c953a5a
Soumen Barick <baricksoumen5@gmail.com>
2026-07-22 23:34:13 +0530

    PrototypalInheritance

---
+++ 11_advjavascript/prototypepalinheritancs.js
@@ -0,0 +1,12 @@
+function Person(name) {
+    this.name = name
+}
+
+Person.prototype.greet = function () {
+    console.log(`Hello my name is ${this.name}`);
+};
+
+let soumen = new Person("Soumen")
+soumen.greet();
+
+// [[Prototype]]
