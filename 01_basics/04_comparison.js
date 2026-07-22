eb0b3069505ad956b3cc84f089f3a2c350c2ece4
Soumen Barick <baricksoumen5@gmail.com>
2026-07-22 23:07:47 +0530

    Basics of Javascript

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1)
+// console.log(2 < 1)
+// console.log(2 = 1)
+// console.log(2 >= 1)
+// console.log(2 != 1)
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+console.log(null > 0);  // null => 0
+console.log(null == 0);
+console.log(null >= 0); // true
+
+console.log(undefined > 0);  // False
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+// === Datatype check hoii 
+
+console.log("2" === 2);
+
---
+++ 01_basics/05_datatype-summary.js
@@ -0,0 +1,49 @@
+// Primitive ==> Call By Value
+
+// 7 types : String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+// Javascript is a dynamically typed Langueage
+
+const score = false
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+
+
+
+
+// Reference Type(Non - Primitive) ==>  Arrays, Objects, Functions
+
+const heros = ["Spiderman", "Doga", "IronMan"];
+let myObj = {
+    name = "Soumen",
+    age = 22,
+}
+
+const myFunction = function () {
+    console.log("Hello");
+}
+
+
+
+
+// Stack(Primitive) and Heap(Non-Primitive) Memory
+
+let userOne = {
+    email : "user@gmail.com" ,
+    upi : "user@ybi"
+}
+
+let userTwo = userOne
+
+userTwo.email = "user2@gamil.com";
+console.log(userOne.email);
+console.log(userTwo.email);
